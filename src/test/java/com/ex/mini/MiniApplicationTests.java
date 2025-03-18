8a450e148218aa50b6e3ad9a26076ca64a38069f
DESKTOP-5360O1P\server <tkdduq241@naver.com>
2025-03-18 14:45:48 +0900

    프로젝트 생성

---
+++ src/test/java/com/ex/mini/MiniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ex.mini;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
