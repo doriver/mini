7a57ac97fb9be8092259d129bc5fb182e33b39d7
DESKTOP-5360O1P\server <tkdduq241@naver.com>
2025-05-06 18:51:47 +0900

    주문까지 Postman으로 테스트 성공

--- src/main/java/com/ex/mini/shop/domain/repository/ItemInCartRepository.java
+++ src/main/java/com/ex/mini/shop/domain/repository/ItemInCartRepository.java
@@ -2,11 +2,14 @@ package com.ex.mini.shop.domain.repository;
 
 import com.ex.mini.shop.domain.entity.ItemInCart;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 public interface ItemInCartRepository extends JpaRepository<ItemInCart, Long> {
 
     List<ItemInCart> findAllByUserId(Long userId);
+
+    @Transactional
     void deleteByUserId(Long userId);
 }
---
+++ src/main/java/com/ex/mini/shop/presentation/api/AAA.java
@@ -0,0 +1,21 @@
+package com.ex.mini.shop.presentation.api;
+
+import com.ex.mini.common.argumentResolver.UserInfo;
+import com.ex.mini.shop.application.CartService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+@RequiredArgsConstructor
+public class AAA {
+    private final CartService cartService;
+
+    @GetMapping("/a1")
+    public void asd(UserInfo userInfo) {
+        cartService.emptyCartAfterOrder(userInfo.getUserId(), null);
+    }
+}
